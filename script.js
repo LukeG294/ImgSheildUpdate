a78205d366fd13d866ec18455f4cdab65d17e71f
Luke Graham <62516707+LukeG294@users.noreply.github.com>
2020-12-14 18:44:42 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function check(){
   var code = prompt("Please enter the passcode:")
   
+  if var code == "Humanities6/7"{
+    alert("Hi, thank you."+
+        "Ryleigh's Daily Tracker: https://docs.google.com/document/d/1GQGsnR2bvUL7JYV0DnSMec4BrTn39CSzo8QD-9NHZz4/edit"+
+        "Luke's Daily Tracker: https://docs.google.com/document/d/1V0UiQKQBOi_Xo55JZo2H2nK9MIDonVR_I8PpP5UUDds/edit")
+  }
+  
+  
 }
